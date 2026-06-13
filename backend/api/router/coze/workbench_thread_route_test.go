7f347725a3be060ea68f5ab2abb1b40345adce01
liuwenbo <840582614@qq.com>
2026-06-13 18:53:00 +0800

    feat: register workbench task thread routes

---
+++ backend/api/router/coze/workbench_thread_route_test.go
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2025 coze-dev Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package coze
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/cloudwego/hertz/pkg/app/server"
+	"github.com/cloudwego/hertz/pkg/common/ut"
+	"github.com/stretchr/testify/require"
+)
+
+func TestRegisterIncludesWorkbenchTaskThreadRoutes(t *testing.T) {
+	h := server.Default()
+	Register(h)
+
+	list := ut.PerformRequest(h.Engine, http.MethodGet, "/api/workbench/task_threads?space_id=1", nil)
+	detail := ut.PerformRequest(h.Engine, http.MethodGet, "/api/workbench/task_threads/1", nil)
+
+	require.NotEqual(t, http.StatusNotFound, list.Code)
+	require.NotEqual(t, http.StatusNotFound, detail.Code)
+}
